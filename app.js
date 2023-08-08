74f80cbccfa4cf7e6bff378c178af998c4c76e5f
Mr-Hera <randellhera@gmail.com>
2023-08-08 12:08:40 +0300

    Feat: added pug templating to render views, added default page layout

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 // allows access to req.body without need for streams & buffers
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,12 +16,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // statically load files i.e. make available css files in public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 
 app.use(shopRoutes);
 
 app.use((req, res, next)=> {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found!'});
 })
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 // router allows to define routes in separate files and import them in app entry point
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    // console.log('shop.js ~', adminData.products);
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+
+    const products = adminData.products;
+    res.render('shop', {prods: products, pageTitle: 'shop', path: '/'});
 })
 
 module.exports = router;
